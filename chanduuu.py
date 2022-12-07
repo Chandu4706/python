7f9a28771a2363ec6ffbab052ab9525334714f8f
chandu <118988870+Chandu4706@users.noreply.github.com>
2022-12-07 09:47:18 +0530

    Add files via upload
    second repositary

---
+++ chanduuu.py
@@ -0,0 +1,11 @@
+import random as a
+li1 = ["About 100 years ago", "Once upon a time"]
+li2 = ["they lived a ghost,", "they lived a king,",
+       "they lived a hero,", "they lived a lion."]
+li3 = ["called as nag", "called as chan",
+       "called as vikram", "called as chandra"]
+li4 = ["he's the most"]
+li5 = ["Dangerous", "kind hearted", "Cruel", "honesty"]
+li6 = ["creature"]
+print(a.choice(li1), a.choice(li2), a.choice(li3),
+      a.choice(li4), a.choice(li5), a.choice(li6))
